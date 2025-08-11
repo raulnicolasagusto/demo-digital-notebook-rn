a0708a0a6f6910a0719bad40fa518ab2ade4bc7d
Raul Agusto <raulnicolasagusto@gmail.com>
2025-08-11 12:54:02 -0300

    React Hook Form and validations using zod

--- App.tsx
+++ App.tsx
@@ -1,9 +1,37 @@
+import { useForm } from 'react-hook-form'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, KeyboardAvoidingView, Platform } from 'react-native';
+import { StyleSheet, KeyboardAvoidingView, Platform, View, Text } from 'react-native';
 import CustomInput from './src/components/CustomInput';
 import CustomButton from './src/components/CustomButton';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 
-export default function App() {
+
+
+const signInSchema = z.object({
+  email: z.string({ message: 'El email es obligatorio' }).email('Email inválido').nonempty('El email es obligatorio'),
+  password: z.string({ message: 'La contraseña es obligatoria' }).min(8, 'La contraseña debe tener al menos 8 caracteres').nonempty('La contraseña es obligatoria'),
+});
+
+//con esta linea podemos agregar el type a la funcion signIn
+type SignInField = z.infer<typeof signInSchema>;
+
+
+export default function SignIn() {
+
+  const { control, handleSubmit, formState: { errors } } = useForm({
+    mode: 'onBlur', // Esto hará que la validación se dispare cuando el usuario pierda el foco
+    resolver: zodResolver(signInSchema)
+  });
+
+
+  const onSignIn = (data: SignInField) => {
+    //Manual validation
+    console.log('Sign in', data.email, data.password);
+  };
+
+  
+
   return (
     <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={styles.container}>
 
@@ -11,10 +39,29 @@ export default function App() {
       <TextInput autoComplete='email' keyboardType="email-address" autoCapitalize="none" placeholder="Email" style={styles.input} />
       <TextInput keyboardType="default" autoCapitalize="none" placeholder="Password" style={styles.input} secureTextEntry /> */}
 
-      <CustomInput autoComplete='email' keyboardType="email-address" autoCapitalize="none" placeholder="Email"/>
-      <CustomInput autoComplete='password' keyboardType="default" autoCapitalize="none" placeholder="Password" secureTextEntry/>
+      <Text style={styles.title}> Sign In</Text>
 
-      <CustomButton text="Sign in" onPress={() => {console.log('Sign in pressed')}}/>
+      <View style={styles.form}>
+        <CustomInput
+        control={control}
+        name="email"
+        keyboardType="email-address"
+        autoCapitalize="none"
+        placeholder="Email"
+        />
+
+        <CustomInput
+        control={control}
+        name="password"
+        autoComplete='password'
+        keyboardType="default"
+        autoCapitalize="none"
+        placeholder="Password"
+        secureTextEntry
+        />
+     </View>
+
+      <CustomButton text="Sign in" onPress={handleSubmit(onSignIn)}/>
 
       <StatusBar style="auto" />
     </KeyboardAvoidingView>
@@ -47,7 +94,13 @@ const styles = StyleSheet.create({
     fontWeight: '600',
     fontSize: 16,
   },
+
+  form:{
+    gap: 5, // Espacio entre los campos del formulario
+  },
+
 });
 
 
 
+// 52:00
--- src/components/CustomInput.tsx
+++ src/components/CustomInput.tsx
@@ -1,17 +1,49 @@
+import { Controller } from 'react-hook-form';
 import { View, Text, TextInput, StyleSheet, TextInputProps } from 'react-native'
 
 
 type CustomInputProps = {
     //custome fields
-    
+    control: any;
+    name: string;
+    rules?: any;
 } & TextInputProps;
 
-export default function CustomeInput(props: CustomInputProps) {
+export default function CustomInput({ control, name, rules = {}, ...props }: CustomInputProps) {
   //poniendo el style despues del props, y agregando al style props.style, podemos con esto, agregar estilos por fuera , es decir dentro del custominput por ej: <CustomInput style{{borderColor:'red'}}/>
-  return <TextInput {...props} style={[styles.input, props.style]} />;
+  
+  // Reglas por defecto si no se proporcionan
+  const defaultRules = {
+    required: 'Este campo es obligatorio',
+    ...rules
+  };
+
+return (
+    <Controller
+       control={control}
+       name={name}
+       render={({ field: { onChange, onBlur, value }, fieldState:{error} }) => (
+        <View style={styles.container}>
+          <TextInput
+            {...props}
+            style={[styles.input, props.style, error && styles.inputError]}
+            onChangeText={onChange}
+            onBlur={onBlur}
+            value={value}
+          />
+          <Text style={styles.error}>{error?.message}</Text>
+        </View>
+      )}
+    />
+    
+  )
 }
 
 const styles = StyleSheet.create({
+  container:{
+    gap:2,
+  },
+
   input: {
     width: '100%',
     padding: 12,
@@ -19,6 +51,15 @@ const styles = StyleSheet.create({
     borderColor: '#ccc',
     borderRadius: 5,
     marginVertical: 8,
+  },
+  inputError: {
+    borderColor: 'crimson',
+    borderWidth: 1,
+  },
+  error: {
+    color: 'crimson',
+    fontSize: 12,
+    marginBottom: 4,
   },
 })
 
